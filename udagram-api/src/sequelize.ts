55e41961224637885c0dcad8321d384fd5b90ff4
Amjad Elmahdy <amjadelmahdy.udacitytraining@gmail.com>
2022-03-24 00:20:18 +0200

    starter

--- udagram-api/src/sequelize.ts
+++ udagram-api/src/sequelize.ts
@@ -1,13 +1,18 @@
 import { Sequelize } from "sequelize-typescript";
 import { config } from "./config/config";
 
-export const sequelize = new Sequelize({
-  username: config.username,
-  password: config.password,
-  database: config.database,
-  host: config.host,
-  port: config.port,
+// export const sequelize = new Sequelize({
+//   username: config.username,
+//   password: config.password,
+//   database: config.database,
+//   host: config.host,
+//   port: config.port,
 
-  dialect: "postgres",
-  storage: ":memory:",
-});
+//   dialect: "postgres",
+//   storage: ":memory:",
+// });
+
+// Just like classroom's code.
+export const sequelize = new Sequelize(
+  "postgres://udgram:password123@amjaddb.cwsazyior4i9.us-east-1.rds.amazonaws.com:5432/udagram"
+);
