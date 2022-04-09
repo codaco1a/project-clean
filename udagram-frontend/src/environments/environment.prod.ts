c6142bd4b73e2a42eb113f77a2ab6afc4d68d747
Amjad Elmahdy <92515003+codaco1a@users.noreply.github.com>
2022-04-09 08:22:25 +0200

    Update environment.prod.ts

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://deployment-project.eba-y2biktwv.us-east-1.elasticbeanstalk.com/api/v0",
+  apiHost: "http://us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
